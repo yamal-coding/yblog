8803266f3643cf13bded4c591820d74255d7c9c8
yamal <yamal.coding@gmail.com>
2020-10-22 17:28:55 +0200

    create common Box component

--- web/src/pages/detail/components/PostDetail.js
+++ web/src/pages/detail/components/PostDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Box from '../../../common/components/Box';
+
 
 const PostDetail = (props) => {
   const { match: { params } } = props;
 
-  return <div>
+  return <Box>
     <h1>This is the detail of the post with id {params.id}</h1>
-  </div>;
+  </Box>;
 }
 
 export default PostDetail;
--- web/src/pages/home/components/PostDetail.js
+++ web/src/pages/home/components/PostDetail.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Box from '../../../common/components/Box';
 
 const PostPreview = ({id, title, description}) => {
-    return <div>
+    return <Box>
       <strong>{title}</strong>
       <p>{description}</p>
       <Link to={`/${id}`}>See full post</Link>
-    </div>;
+    </Box>;
   };
 
 
